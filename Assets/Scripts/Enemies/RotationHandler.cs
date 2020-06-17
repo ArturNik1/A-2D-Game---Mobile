9ddceee3e42a9f76e6b34eb2f792991c77d90c89
arturnik17 <Artur1qaz2wsx>
2020-06-17 17:12:18 +0300

    V0.168
    Added:
    ~ RPG Monster Wave02 Asset Pack.
    ~ Chest enemy.
    ~ Rotation is done via script to the Chest.
    
    Former-commit-id: 190c2a53031bec1a4ce72399264dc6a2fe97179b

---
+++ Assets/Scripts/Enemies/RotationHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationHandler : MonoBehaviour
+{
+    public Vector2 direction;
+    GameObject lockedPlayer;
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lockedPlayer = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = new Vector3(transform.position.x, transform.position.y, lockedPlayer.transform.position.z);
+        float posX = -(pos - lockedPlayer.transform.position).normalized.x, posY = -(pos - lockedPlayer.transform.position).normalized.y;
+        direction = new Vector2(posX, posY);
+        HandleRotation();
+    }
+
+    void HandleRotation() {
+        float x = GetEularXRotationFromDirection();
+        float y = GetEularYRotationFromDirection();
+        float z = GetEularZRotationFromDirection();
+        if (x == 0 && y == 0 && z == 0) return;
+        transform.rotation = Quaternion.Euler(x, y, z);
+    }
+
+    float GetEularXRotationFromDirection() {
+        if (direction.y >= 0) return (-50 * Mathf.Pow(direction.y, 2));
+        else if (direction.y < 0) return (50 * Mathf.Pow(direction.y, 2));
+
+        Debug.LogWarning("Rotation is invalid!");
+        return 0f;
+    }
+
+    float GetEularYRotationFromDirection() {
+        float rotY = (31.1f * direction.x * (1.4f * Mathf.Pow(direction.x, 2) + 1));
+
+        if (direction.y >= 0) return rotY;
+        else if (direction.x >= 0) return (1 - (rotY / 75f)) * 180 + rotY; 
+        else if (direction.x < 0)  return -((1 + (rotY / 75f)) * 180 - rotY);
+
+        Debug.LogWarning("Rotation is invalid!");
+        return 0f;
+    }
+
+    float GetEularZRotationFromDirection() {
+        return (-37.3f * direction.x * (1.4f * Mathf.Pow(direction.x, 2) + 1));
+    }
+
+}
